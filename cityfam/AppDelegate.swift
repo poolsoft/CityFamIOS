2bc1869c75c4d17551f5c8b2ed7f8fea2ff3e7a3
i mark <imark@is-Mac-mini.local>
2017-05-25 19:48:24 +0530

    Worked on Home Page

--- cityfam/AppDelegate.swift
+++ cityfam/AppDelegate.swift
@@ -11,12 +11,19 @@ import FBSDKLoginKit
 import FacebookCore
 import Google
 import GoogleSignIn
+import UserNotifications
+
+import Firebase
+import FirebaseInstanceID
+import FirebaseMessaging
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate,GIDSignInUIDelegate {
 
     var window: UIWindow?
     var hud:MBProgressHUD!
+
+    let gcmMessageIDKey = "gcm.message_id"
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
@@ -41,6 +48,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate,GIDSignInUIDelegate {
 //        
 //        GIDSignIn.sharedInstance().clientID = "59303853655-jl9hdqnu3mu5e7u2i8mbgfdsk9di5c06.apps.googleusercontent.com"
         //GIDSignIn.sharedInstance().delegate = self
+        
+        // [START register_for_notifications]
+        if #available(iOS 10.0, *) {
+            let authOptions: UNAuthorizationOptions = [.alert, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+            
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            // For iOS 10 data message (sent via FCM)
+            FIRMessaging.messaging().remoteMessageDelegate = self
+            
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        
+        // [END register_for_notifications]
+        FIRApp.configure()
+        
+        // Add observer for InstanceID token refresh callback.
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.tokenRefreshNotification),
+                                               name: .firInstanceIDTokenRefresh,
+                                               object: nil)
         
         return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     }
@@ -94,6 +130,98 @@ class AppDelegate: UIResponder, UIApplicationDelegate,GIDSignInUIDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        // If you are receiving a notification message while your app is in the background,
+        // this callback will not be fired till the user taps on the notification launching the application.
+        // TODO: Handle data of notification
+        // Print message ID.
+        
+        application.applicationIconBadgeNumber = 0
+        
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        print("\(userInfo)")
+        // Print full message.
+        
+        
+        
+        if let aps = userInfo["aps"] as? NSDictionary {
+            if let alert = aps["alert"] as? NSDictionary {
+                if let title = alert["title"] as? NSString {
+                    if let body = alert["body"] as? NSString {
+                        CommonFxns.showAlert((window?.rootViewController)!, message: "\(title) \n \(body)", title: "Notification")
+                    }
+                }
+            }
+        }
+        
+        
+        
+        completionHandler(UIBackgroundFetchResult.newData)
+    }
+    // [END receive_message]
+    // [START refresh_token]
+    
+    func tokenRefreshNotification(_ notification: Notification) {
+        if let refreshedToken = FIRInstanceID.instanceID().token() {
+            print("InstanceID token: \(refreshedToken)")
+            UserDefaults.standard.setValue(refreshedToken, forKey: "tokenId")
+            if UserDefaults.standard.value(forKey: "refreshTokenID") == nil{
+                if(UserDefaults.standard.value(forKey: "userId") != nil){
+                    NotificationCenter.default.post(name: NSNotification.Name(rawValue: "registerTokenIdNotification"), object: nil)
+                }
+            }
+        }
+        // Connect to FCM since connection may have failed when attempted before having a token.
+        connectToFcm()
+    }
+    
+    // [END refresh_token]
+    // [START connect_to_fcm]
+    func connectToFcm() {
+        // Won't connect since there is no token
+        guard FIRInstanceID.instanceID().token() != nil else {
+            return;
+        }
+        
+        // Disconnect previous FCM connection if it exists.
+        FIRMessaging.messaging().disconnect()
+        
+        FIRMessaging.messaging().connect { (error) in
+            if error != nil {
+                print("Unable to connect with FCM. \(error)")
+            } else {
+                print("Connected to FCM.")
+            }
+        }
+    }
+
+    //    // This function is added here only for debugging purposes, and can be removed if swizzling is enabled.
+    //    // If swizzling is disabled then this function must be implemented so that the APNs token can be paired to
+    //    // the InstanceID token.
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNs token retrieved: \(deviceToken)")
+        
+        //         With swizzling disabled you must set the APNs token here.
+        FIRInstanceID.instanceID().setAPNSToken(deviceToken, type: FIRInstanceIDAPNSTokenType.sandbox)
+        
+        if let token = FIRInstanceID.instanceID().token() {
+            print("InstanceID token: \(token)")
+            UserDefaults.standard.setValue(token, forKey: "tokenId")
+            
+            if UserDefaults.standard.value(forKey: "refreshTokenID") == nil{
+                if(UserDefaults.standard.value(forKey: "userId") != nil){
+                    if (UserDefaults.standard.value(forKey: "refreshTokenID") as! String) !=  token{
+                        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "registerTokenIdNotification"), object: nil)
+                    }
+                }
+            }
+        }
+        
+    }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
@@ -121,8 +249,53 @@ class AppDelegate: UIResponder, UIApplicationDelegate,GIDSignInUIDelegate {
     func hideProgressHUD(view : UIView)->Void{
         MBProgressHUD.hide(for: view, animated: true)
     }
+}
+
+@available(iOS 10, *)
+extension AppDelegate : UNUserNotificationCenterDelegate {
     
-
-
+    // Receive displayed notifications for iOS 10 devices.
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        
+        // Print full message.
+        print(userInfo)
+        
+        //        let userInfoDict = notification.request.content.userInfo as? NSDictionary
+        
+        
+        
+        
+        // Change this to your preferred presentation option
+        completionHandler([])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        // Print message ID.
+        if let messageID = userInfo[gcmMessageIDKey] {
+            print("Message ID: \(messageID)")
+        }
+        // Print full message.
+        print(userInfo)
+        
+        completionHandler()
+    }
+}
+// [END ios_10_message_handling]
+// [START ios_10_data_message_handling]
+extension AppDelegate : FIRMessagingDelegate {
+    // Receive data message on iOS 10 devices while app is in the foreground.
+    func applicationReceivedRemoteMessage(_ remoteMessage: FIRMessagingRemoteMessage) {
+        print(remoteMessage.appData)
+}
 }
 
